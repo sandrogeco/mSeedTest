84a6c106d9afa731e0390f6b150ee5eadb55791d
Unknown <sandrovezzosi@gmail.com>
2020-07-10 08:39:54 +0000

    parallel

--- drumPlotHyst.py
+++ drumPlotHyst.py
@@ -3,15 +3,22 @@ import numpy
 import sys
 import time
 from obspy import UTCDateTime
-#from obspy.clients.filesystem.sds import Client
+from obspy.clients.filesystem.sds import Client
 from obspy.clients.fdsn import Client
 from matplotlib.backends.backend_agg import FigureCanvasAgg
-#from obspy.clients.seedlink.basic_client import Client
+# from obspy.clients.seedlink.basic_client import Client
 from obspy.clients.seedlink.easyseedlink import create_client
 from obspy.clients.seedlink.easyseedlink import EasySeedLinkClient
 from obspy.core.stream import Stream
+from obspy import read
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+from obspy import read_inventory
 
+import threading
+from threading import Thread
+
+thread=Thread()
 
 import png
 
@@ -19,55 +26,68 @@ import paramiko
 
 from PIL import Image
 
+dpi = 100
+sizex = 800
+sizey = 600
+yRange = 0.1
 
+hystType = [1440, 360, 180, 60]
 
-dpi=100
-sizex=800
-sizey=600
-
-hystType = [1440,360,180]
-
-band={
-    'low':[0.05,0.1],
-    'high':[0.1,0.5]
+band = {
+    'low': [1, 20],
+    'high': [20, 50]
 }
 
 rTWindow = 360
-rtSft=2
+rtSft = 2
 
-
-ssh_client =paramiko.SSHClient()
+ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname='80.211.98.179', username='braskem', password='Geoapp2020!')
-sftp=ssh_client.open_sftp()
+sftp = ssh_client.open_sftp()
+inv = read_inventory("metadata/Braskem_metadata.xml")
+
 
 
 
 class MyClient(EasySeedLinkClient):
+    _traces =Stream()
+    _inv = inv
+    _rtSft = rtSft
+    _lastData = UTCDateTime.now()
     _traces = Stream()
-    _lastData=UTCDateTime.now()
-    _rTWindow=rTWindow
-    _rtSft=rtSft
+    _appTrace = Stream()
+    _rTWindow = rTWindow
 
-    def plotDrum(self,tr):
+    def __init__(self,ip):
+        EasySeedLinkClient.__init__(self,ip)
+        #self.hyDr = drThread('H')
+        #self.rtDr =drThread('RT')
 
+    def plotDrum(self, filename='tmp.png'):
         try:
-            im=tr.plot(type='dayplot',
-                    dpi=dpi,
-                    x_labels_size=int(8 * 100 / int(dpi)),
-                    y_labels_size=int(8 * 100 / int(dpi)),
-                    title_size=int(1000 / int(dpi)),
-                    size=(sizex, sizey),
-                    color=('#AF0000', '#00AF00', '#0000AF'),
-                    # transparent=True,
-                    handle=True
-                    # bgcolor='black',
-                    # grid_color='white',
-                    # face_color='black',+
-                    # show_y_UTC_label=False,
-                    #outfile='tmp.png'
-                    )
-            im.savefig('tmp.png')
+            self._appTrace.data = self._appTrace.data * 1000
+            im = self._appTrace.plot(type='dayplot',
+                                     dpi=dpi,
+                                     x_labels_size=int(8 * 100 / int(dpi)),
+                                     y_labels_size=int(8 * 100 / int(dpi)),
+                                     title_size=int(1000 / int(dpi)),
+                                     size=(sizex, sizey),
+                                     color=('#AF0000', '#00AF00', '#0000AF'),
+                                     # right_vertical_labels=True,
+
+                                     vertical_scaling_range=yRange,
+                                     # transparent=True,
+                                     handle=True,
+                                     time_offset=-3,
+                                     data_unit='mm/s'
+                                     # bgcolor='black',
+                                     # grid_color='white',
+                                     # face_color='black',+
+                                     # show_y_UTC_label=False,
+                                     # outfile='tmp.png'
+                                     )
+            im.savefig(filename)
             plt.close(im)
 
             return True
@@ -75,55 +95,103 @@ class MyClient(EasySeedLinkClient):
             print('ops,something wrong in plotting!!')
             return False
 
-    def realTimeDrumPlot(self,b):
+    def realTimeDrumPlot(self):
+
         for tr in self._traces:
-            id=tr.get_id()
-            spl=id.split('.')
-            network=spl[0]
-            station=spl[1]
-            channel=spl[3]
-            for band in b:
-                fileNameRT = 'RT_' + network + '_' + station + '_' + channel + '_' + str(band) + '.png'
-                trApp=tr.copy()
-                bb=b[band]
-                trApp.trim(UTCDateTime.now()-self._rTWindow*60,UTCDateTime.now())
-                trApp.filter('bandpass',freqmin=bb[0],freqmax=bb[1],corners=2,zerophase=True)
-                if self.plotDrum(trApp):
+            id = tr.get_id()
+            print('rt '+id)
+            spl = id.split('.')
+            network = spl[0]
+            station = spl[1]
+            channel = spl[3]
+            for b in band:
+                fileNameRT = 'RT_' + network + '_' + station + '_' + channel + '_' + str(b) + '.png'
+                self._appTrace = tr.copy()
+                bb = band[b]
+                self._appTrace.trim(UTCDateTime.now() - self._rTWindow * 60, UTCDateTime.now())
+                self._appTrace.filter('bandpass', freqmin=bb[0], freqmax=bb[1], corners=2, zerophase=True)
+                if self.plotDrum():
+                    sftpMkdirs(sftp, '/RT/', 'uploads')
                     sftp.put('tmp.png', 'uploads/RT/' + fileNameRT)
-                print(fileNameRT)
+                #print(fileNameRT)
 
+    def hystDrumPlot(self):
 
+        for tr in self._traces:
+            id = tr.get_id()
+            print('hyst '+id)
+            spl = id.split('.')
+            network = spl[0]
+            station = spl[1]
+            channel = spl[3]
+            tEnd = UTCDateTime.now()
 
-    def on_data(self,traces):
+            for h in hystType:
+
+                if tEnd.hour % int(h / 60) == 0:
+                    for b in band:
+                        tStart = tEnd - h * 60
+                        p = network + '/' + station + '/' + channel + '/' + str(tEnd.year) + '/' + str(
+                            tEnd.month) + '/' + str(
+                            tEnd.day) + '/' + str(h) + '/' + str(b)
+
+                        fileName = p + '/' + tStart.strftime("%Y%m%d%H%M%S") + '_' + tEnd.strftime(
+                            "%Y%m%d%H%M%S") + '.png'
+
+                        self._appTrace = tr.copy()
+                        bb = band[b]
+                        self._appTrace.trim(tStart, tEnd)
+                        self._appTrace.filter('bandpass', freqmin=bb[0], freqmax=bb[1], corners=2, zerophase=True)
+                        if self.plotDrum('tmpH.png'):
+                            sftpMkdirs(sftp, p, 'uploads')
+                            sftp.put('tmpH.png', 'uploads/' + fileName)
+                        #print(fileName)
+
+    def align(self):
+        if os.path.exists('traces.mseed'):
+            self._traces = read('traces.mseed')
+
+    def save(self):
+        print('saving')
+        self._traces.write('traces.mseed')
+        print('saved')
+
+    def on_data(self, traces):
         # if len(traces)==0:
         #     for trace in traces:
         #         trace
-        tEnd=UTCDateTime.now()
-        self._traces+=traces
-        self._traces.merge()
-        self._traces.trim(tEnd-1440*60,tEnd)
-        print(self._traces)
+        tEnd = UTCDateTime.now()
+        traces.remove_response(self._inv)
+        self._traces += traces
+        self._traces.merge(fill_value=0)
+        self._traces.trim(tEnd - 1440 * 60, tEnd)
+        print(tEnd)
+        #print(self._traces)
         if (tEnd.minute % self._rtSft == 0) & (self._lastData.minute % self._rtSft != 0):
-            self.realTimeDrumPlot(band)
+        #if (tEnd.second % 30 == 0): #& (self._lastData.minute % self._rtSft != 0):
+            # self.realTimeDrumPlot
+            #self.rtDr.start(self._traces)
+            #self.realTimeDrumPlot()
+
+            rtThread = Thread(target=self.realTimeDrumPlot)
+            rtThread.start()
+            sThread=Thread(target=self.save)
+            sThread.start()
+
+        if (tEnd.minute == 0) & (self._lastData.minute != 0):
+            #self.hyDr.start(self._traces)
+            #self.hystDrumPlot()
+            hyThread = Thread(target=self.hystDrumPlot)
+            hyThread.start()
+            # self.hystDrumPlot()
         self._lastData = tEnd
 
 
+client = MyClient('172.16.8.10')
 
 
 
-client= MyClient('172.16.8.10')
-
-# data=[('IU', 'ANMO', '', 'LHZ'),('IU', 'ANMO', '', 'LH1'),('IU', 'ANMO', '', 'LH2'),
-#       ('IU', 'AFI', '', 'LHZ'),('IU', 'AFI', '', 'LH1'),('IU', 'AFI', '', 'LH2'),
-#       ('IU', 'ADK', '', 'LHZ'),('IU', 'ADK', '', 'LH1'),('IU', 'ADK', '', 'LH2'),
-#       ('IU', 'COR', '', 'LHZ'),('IU', 'COR', '', 'LH1'),('IU', 'COR', '', 'LH2'),
-#       ('IU', 'COLA', '', 'LHZ'),('IU', 'COLA', '', 'LH1'),('IU', 'COLA', '', 'LH2')]
-# data=[('MN', 'AQU', '', 'BHE'),('MN', 'AQU', '', 'BHN'),('MN', 'AQU', '', 'BHZ')]
-data=[('LK', 'BRK0', 'EHE'),('LK', 'BRK0', 'EHN'),('LK', 'BRK0', 'EHZ'),
-      ('LK', 'BRK1', 'EHE'),('LK', 'BRK1', 'EHN'),('LK', 'BRK1', 'EHZ')]
-
-
-def sftpExist(p,path):
+def sftpExist(p, path):
     try:
         p.stat(path)
         return True
@@ -132,73 +200,15 @@ def sftpExist(p,path):
         return False
 
 
-def sftpMkdirs(p,path,basePath):
-    dirs=path.split('/')
-    parPath=basePath
+def sftpMkdirs(p, path, basePath):
+    dirs = path.split('/')
+    parPath = basePath
     for dir in dirs:
-        parPath+='/'+dir
-        if not sftpExist(p,parPath):
+        parPath += '/' + dir
+        if not sftpExist(p, parPath):
             p.mkdir(parPath)
 
 
-hst=True
-
-
-def drum(st,tEnd,hyst,band,b):
-    network = st[0]
-    station = st[1]
-    channel = st[3]
-
-    tStart = tEnd - hyst * 60
-    p = network + '/' + station + '/' + channel + '/' + str(tEnd.year) + '/' + str(tEnd.month) + '/' + str(
-        tEnd.day) + '/' + str(hyst)+'/'+str(b)
-
-    sftpMkdirs(sftp, p, 'uploads')
-    sftpMkdirs(sftp, '/RT/', 'uploads')
-
-    traces = client.get_waveforms(network, station, "*", channel, tStart, tEnd)
-    for tr in traces:
-        trId = tr.get_id()
-
-        fileName = p + '/' + tStart.strftime("%Y%m%d%H%M%S") + '_' + tEnd.strftime("%Y%m%d%H%M%S") + '.png'
-        fileNameRT='RT_'+ network + '_' + station + '_' + channel + '_' + str(b)+'.png'
-
-        tr.filter('bandpass',freqmin=band[0],freqmax=band[1],corners=2,zerophase=True)
-        tr.plot(type='dayplot',
-                dpi=dpi,
-                x_labels_size=int(8 * 100 / int(dpi)),
-                y_labels_size=int(8 * 100 / int(dpi)),
-                title_size=int(1000 / int(dpi)),
-                size=(sizex, sizey),
-                color=('#AF0000', '#00AF00', '#0000AF'),
-                #transparent=True,
-                #handle=True
-                # bgcolor='black',
-                # grid_color='white',
-                # face_color='black',+
-                # show_y_UTC_label=False,
-                outfile='tmpmpl.png'
-                )
-        im = Image.open('tmpmpl.png')
-        #Image.fromarray(numpy.fromstring(imM.canvas.tostring_rgb(), dtype=numpy.uint8).reshape(lst)).convert('P', palette=Image.AFFINE).save('tmp.png', format='PNG',optimization=True)
-  #      Image.fromarray(X,'RGBA').convert('P', palette=Image.AFFINE).save('tmp.png', format='PNG',optimization=True)
-        im.convert('RGBA').convert('P', palette=Image.AFFINE).save('tmp.png', format='PNG',optimization=True)
-        print(fileNameRT+' '+fileName)
-        return (fileName,fileNameRT)
-
-
-
-def readTraces(tr,traces,tStart,tEnd):
-    i=0
-    for id in tr:
-        traces[i]=client.get_waveforms(id[0],id[1],'*',id[2],tStart,tEnd).append(traces[i])
-        i+=1
-
-
-tOld=UTCDateTime.now()
-test=True
-#traces=client.get_waveforms(data[0],data[1],'*',data[2],tOld-rtSft*60,tOld)
-traces={}
-
-client.select_stream('LK','BRK?','E??')
+client.select_stream('LK', 'BRK?', 'E??')
+client.align()
 client.run()
